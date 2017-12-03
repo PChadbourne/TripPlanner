f061f0d89d49ebb04dc39e666bad526f9f1803ac
PChadbourne <enjayves@gmail.com>
2017-12-03 14:19:02 -0800

    Initial Commit
    Created files and added header comments

---
+++ tests/test_tripPlanner.py
@@ -0,0 +1,6 @@
+
+# Unit tests for Trip Planner iTrellis programming challenge
+#
+# Run tripPlanner.py with commandline argument "test" to run
+#
+# Patrick Chadbourne, December 3rd, 2017
---
+++ tripPlanner.py
@@ -0,0 +1,26 @@
+# Trip Planner iTrellis programming challenge
+# # # # # # # # # # # # # # # # # # # # # # # #
+# Computes from a list of students and their
+# personal expenses from the trip, the minimum
+# amount of money that must change hands in
+# order to equalize all the expenses.
+#
+# Input is taken from the expenses file, with
+# each line representing a different student's
+# expenses, seperated by whitespace.
+# Up to 26 lines can be used in the file, hands
+# each student will be assigned an alphabetical
+# name from A-Z to maintain ease of use.
+#
+# Output is a printed string of required transactions
+# necessary to balance the expenses of each student.
+#
+# Running the program with the commandline argument
+# "test" will run the program's unit tests.
+#
+# Patrick Chadbourne, December 3rd, 2017
+
+def main():
+
+if __name__ == '__main__':
+    main()
